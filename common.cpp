4b302a83be7ef555bf69951be1fd0a53da40bdf3
Eric Holk <eholk@cs.indiana.edu>
2015-09-15 15:48:43 -0400

    Adding my initial benchmarking stuff.

---
+++ common.cpp
@@ -0,0 +1,69 @@
+#include "common.hpp"
+
+#include <iostream>
+
+using namespace std;
+
+uint64_t nanotime() {
+    uint64_t ns_per_s = 1000000000LL;
+    timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    return ((uint64_t)ts.tv_sec * (uint64_t)ns_per_s + (uint64_t)ts.tv_nsec);
+}
+
+
+Benchmark::Benchmark(int num_trials)
+	: mNumTrials(num_trials), mTotalTime(0)
+{}
+
+/**
+   Runs a benchmark.
+
+   It runs it a couple of times just to warm everything up, and then
+   does num_trials iterations of the benchmark for time.
+ */
+void Benchmark::run() {
+	setup();
+
+	// Warm up.
+	run_iteration();
+	run_iteration();
+	finish_iteration();
+
+	// okay, now for time.
+	uint64_t start1 = nanotime();
+	for(int i = 0; i < mNumTrials; ++i) {
+		finish_iteration();
+	}
+	uint64_t stop1 = nanotime();
+
+	uint64_t start2 = nanotime();
+	for(int i = 0; i < mNumTrials; ++i) {
+		run_iteration();
+		finish_iteration();
+	}
+	uint64_t stop2 = nanotime();
+
+	mTotalTime = (stop2 - start2) - (stop1 - start1);
+	
+	cleanup();
+}
+
+double Benchmark::timePerIterationMicros() const {
+	return double(mTotalTime) / (mNumTrials * 1000);
+}
+
+double Benchmark::timePerIteration() const {
+	return double(mTotalTime) / mNumTrials / 1e9;
+}
+
+void Benchmark::setup() {}
+void Benchmark::cleanup() {}
+
+void Benchmark::setNumTrials(int num) {
+	mNumTrials = num;
+}
+
+int Benchmark::getNumTrials() const {
+	return mNumTrials;
+}
---
+++ common.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <inttypes.h>
+#include <time.h>
+
+uint64_t nanotime();
+
+/**
+   Base class for benchmarks. Includes stuff for automatically
+   running, making statistics, etc.
+ */
+class Benchmark {
+public:
+	Benchmark(int num_trials = 10);
+	
+	void run();
+
+	virtual void setup();
+	virtual void cleanup();
+
+	double timePerIteration() const;
+	double timePerIterationMicros() const;
+
+	void setNumTrials(int num_trials);
+	int getNumTrials() const;
+	
+protected:
+	// These are the things that each benchmark needs to implement.
+	
+	virtual void run_iteration() = 0;
+	virtual void finish_iteration() = 0;
+
+private:
+	int mNumTrials;
+	int64_t mTotalTime;
+};
